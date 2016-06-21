11bc3c02a0b4221d590c1610dce86158d805fd2e
ueno-tr <t-ueno@umin.ac.jp>
2016-06-21 14:12:41 +0900

    160621-1

--- DAM_compress.R
+++ DAM_compress.R
@@ -1,5 +1,6 @@
 ##DAMの生データから最後10日分のみを切り出す
 ##ファイルが長い時、多数のファイルを同時に処理する時に軽くできる
+##txtファイルとして出力　DAM file scanを用いる場合はこちら
 
 files <- list.files() #ディレクトリ内のファイル名をfilesに代入
 
@@ -13,4 +14,23 @@ for (file.name in files) {
     c <- a[(b-14400):b,] #最後10日分のデータのみ切り取る
 
     write.table(c, file.name, col.names=FALSE, row.names=FALSE, sep="\t", quote=FALSE)
+    }
+
+
+
+##下記はRDSとして保存する場合　データ容量が軽くなる
+##データフレームとなる
+##この後もRで解析できればこちら
+files <- list.files() #ディレクトリ内のファイル名をfilesに代入
+
+for (file.name in files) {
+    if (regexpr('.txt$', file.name)  < 0) { # ファイル名の最後が '.txt'か？
+        next                                 # そうでなければスキップ．
+    }
+
+    a <- read.table(file.name, sep="\t") #テキストデータ読み込み
+    b <- length(a[,1]) #aの行数
+    c <- a[(b-14400):b,] #最後10日分のデータのみ切り取る
+
+    saveRDS(c, paste(strtrim(file.name,9), ".rds", sep=""))
     }
---
+++ Mall to array.R
@@ -0,0 +1,36 @@
+##複数モニターのデータを三次元配列（array）にするか
+##4320行、32列、モニター数の三次元配列
+##3次元目を指定する名前をファイル名であるモニター番号（M10 etc）とするか
+##DAM file scanで3日分など切り出し後に使用
+
+
+importMarray <- function(n) {#データの日数をnで入れる
+
+files <- list.files() #ディレクトリ内のファイル名をfilesに代入
+
+t <- array(0, dim=c(1440*n, 32, length(files)))
+
+##filesの名前をM022などにする（1604282226CtM022.txtと言う名前を想定）
+##後ろから8文字目~後ろから5文字目
+for (i in 1:length(files)){
+files[i] <- substring(files[i],nchar(files[i])-7,nchar(files[i])-4)
+}
+
+dimnames(t) <- list(rownames(t), colnames(t), files)
+
+for (file.name in files) {
+
+    if (regexpr('.txt$', file.name)  < 0) { # ファイル名の最後が '.txt'か？
+        next                                 # そうでなければスキップ．
+    }
+
+a <- data.matrix(read.table(file.name, sep="\t")) #文字などが入っているためscanが使えない　44列データになる
+b <- a[,11:42] #最初12列を削除
+colnames(b) <- NULL #列の名前を取り除く
+
+t[,,file.name] <- b
+
+}
+
+return(t)
+}
