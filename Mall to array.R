aea514c04e8d5e88e69936293a44f38e51de611a
ueno-tr <t-ueno@umin.ac.jp>
2016-06-21 14:30:51 +0900

    Marry

--- Mall to array.R
+++ Mall to array.R
@@ -10,6 +10,22 @@ files <- list.files() #ディレクトリ内のファイル名をfilesに代入
 
 t <- array(0, dim=c(1440*n, 32, length(files)))
 
+
+for (i in 1:length(files)) {
+  file.name <- files[i]
+
+    if (regexpr('.txt$', file.name)  < 0) { # ファイル名の最後が '.txt'か？
+        next                                 # そうでなければスキップ．
+    }
+
+a <- data.matrix(read.table(file.name, sep="\t")) #文字などが入っているためscanが使えない　44列データになる
+b <- a[,11:42] #最初10列を削除
+colnames(b) <- NULL #列の名前を取り除く
+
+t[,,i] <- b
+
+}
+
 ##filesの名前をM022などにする（1604282226CtM022.txtと言う名前を想定）
 ##後ろから8文字目~後ろから5文字目
 for (i in 1:length(files)){
@@ -17,20 +33,6 @@ files[i] <- substring(files[i],nchar(files[i])-7,nchar(files[i])-4)
 }
 
 dimnames(t) <- list(rownames(t), colnames(t), files)
-
-for (file.name in files) {
-
-    if (regexpr('.txt$', file.name)  < 0) { # ファイル名の最後が '.txt'か？
-        next                                 # そうでなければスキップ．
-    }
-
-a <- data.matrix(read.table(file.name, sep="\t")) #文字などが入っているためscanが使えない　44列データになる
-b <- a[,11:42] #最初12列を削除
-colnames(b) <- NULL #列の名前を取り除く
-
-t[,,file.name] <- b
-
-}
 
 return(t)
 }
