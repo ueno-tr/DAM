133bc5eba3341767cf33199075d302d8095af696
ueno-tr <t-ueno@umin.ac.jp>
2016-06-22 15:42:16 +0900

    all

--- summary/all.R
+++ summary/all.R
@@ -235,7 +235,7 @@ barplotall <- function(x) {
 
   ##グラフに出力
   library(ggplot2)
-  p <- ggplot(out, aes(x = time, y = mean,group=genotype, colour=genotype) ) + geom_line()
+  p <- ggplot(out, aes(x = time, y = mean,group=genotype, colour=genotype) ) + geom_line() + ylab("sleep (min/hr)")
 
   errors <- aes(ymax = mean + SEM, ymin = mean - SEM)
   p <- p + geom_errorbar(errors, width = 0.2) + geom_point(size = 2)
