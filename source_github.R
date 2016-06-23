517b7fc3066a2f598d5290497513b1fd00889eb9
ueno-tr <t-ueno@umin.ac.jp>
2016-06-23 20:51:47 +0900

    summary

--- source_github.R
+++ source_github.R
@@ -14,15 +14,14 @@ source_github("https://raw.githubusercontent.com/ueno-tr/DAM/master/summary/all.
 
 ##githubからsourceする
 ##evalのオプションが違うもの　こちらだと読み込んだ関数が使えるか
+
+
 source_github <- function(u) {
-   # load package
    require(RCurl)
-   # read script lines from website and evaluate
    script <- getURL(u, ssl.verifypeer = FALSE)
     eval(parse(text = script),envir=.GlobalEnv)
  }
 
-##使い方
 source_github("https://raw.githubusercontent.com/ueno-tr/DAM/master/summary/DAM%20function%20all.R")
 
 source_github("https://raw.githubusercontent.com/ueno-tr/DAM/master/summary/all.R")
---
+++ summary/install.R
@@ -0,0 +1,3 @@
+##Rを初めて起動した状態からDAMの解析ができる状態を整える
+
+install.packages("ggplot2")
