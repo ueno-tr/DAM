08fa16249a77a80069412bf94e316e1826d6d0f1
ueno-tr <t-ueno@umin.ac.jp>
2016-06-25 22:38:56 +0900

    summary.pdf

--- summary/DAM analysis.R
+++ summary/DAM analysis.R
@@ -16,8 +16,11 @@ out <- meanSEM(hrs, summary)
 
 ##グラフに出力
 library(ggplot2)
+pdf(paste("summary", ".pdf", sep=""), width=8, height=2)
+
 p <- ggplot(out, aes(x = time, y = mean,group=genotype, colour=genotype) ) + geom_line() + ylab("sleep (min/hr)")
 
 errors <- aes(ymax = mean + SEM, ymin = mean - SEM)
 p <- p + geom_errorbar(errors, width = 0.2) + geom_point(size = 2)
 p
+dev.off()
